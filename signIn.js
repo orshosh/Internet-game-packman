3a1062b738f75a421200110eacb11814b4dafc0b
OrShoshani <or.shoshani27@gmail.com>
2020-04-21 22:35:04 +0300

    test

--- signIn.js
+++ signIn.js
@@ -4,6 +4,7 @@ var passwords = new Array();
 $(document).ready(function() {
     users[0]="p";
     passwords[0]="p";
+    $("registerPage").urlParams.append('page',1);
 
     
 });
@@ -17,7 +18,10 @@ function checkInput(){
               required: true,
               onlyLetters:true
           },
-          userName: "required",
+          userName: {
+              required: true,
+              enterValueToArray: true
+          },
           email: {
             required: true,
             email: true
@@ -25,7 +29,8 @@ function checkInput(){
           psw: {
             required: true,
             minlength: 6,
-            lettersAndNumbers: true
+            lettersAndNumbers: true,
+            enterPassToArray: true
           },
           psw_repeat: {
               required: true,
@@ -57,21 +62,16 @@ function checkInput(){
           }
     
       });
-
-      let value1 = $("#userName").val(); 
-      users[1]=value1;
-      let password2 = $("#psw").val();
-      passwords[1]=password2;
     
 }
 
 function checkUser(){
-    
+
+    var p2 = document.getElementsByName("psw2");
     $("#logR").validate({
         rules: {
             userName2:{
-                // params: ["#psw2"],   
-                userExists: ["userName2","#psw2"]
+                userExists: ["pr"]
             }
         },
     
@@ -83,9 +83,9 @@ function checkUser(){
     
 }
 
-
 function register() 
 {
+
  $("#open").css("display", "none");
  $("#loginPage").css("display","none");
  $("#registerPage").css("display","initial");
@@ -122,3 +122,11 @@ $.validator.addMethod('userExists',function(value,element,params){
     return true;
 },
 'Wrong user name or password')
+
+$.validator.addMethod('enterValueToArray',function(value,elemnt){
+    users.push(value);
+   
+});
+$.validator.addMethod('enterPassToArray',function(value,elemnt){
+    passwords.push(value);
+});
